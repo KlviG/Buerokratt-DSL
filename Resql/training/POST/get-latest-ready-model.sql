8f6abb13aba15a6e123989b8da5032c88d187a25
Varmo <101868197+varmoh@users.noreply.github.com>
2024-06-04 09:37:02 +0300

    Create get-latest-ready-model.sql

---
+++ Resql/training/POST/get-latest-ready-model.sql
@@ -0,0 +1,14 @@
+SELECT 
+  model_type,
+  state,
+  trained_date,
+  file_name,
+  version_number,
+  model_version, 
+  test_report,
+  cross_validation_report,
+  created,
+  training_data_checksum
+FROM llm_trainings
+WHERE trained_date = (SELECT max(trained_date) FROM llm_trainings WHERE state = 'READY')
+LIMIT 1;
